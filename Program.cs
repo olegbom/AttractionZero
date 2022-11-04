64a6f00f8d9560f4796a24d96fcd2c4b241666b7
Высоцкий Олег <Olegbom@gmail.com>
2022-11-04 15:43:01 +0300

    Start Triangle Life

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace AttractionZero
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+using Raylib_cs;
+
+namespace AttractionZero
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TriangleLife ts = new(25*5, 12*5);
+            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT);
+
+            Raylib.InitWindow(1600, 1000, "Hello World");
+            bool isEveryFrame = false;
+            while (!Raylib.WindowShouldClose())
+            {
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_S)) ts.Step();
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_P)) isEveryFrame = !isEveryFrame;
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE)) ts = new(25 * 5, 12 * 5);
+                
+                if (isEveryFrame) ts.Step();
+                
+                Raylib.BeginDrawing();
+               
+                Raylib.ClearBackground(Color.WHITE);
+                Raylib.DrawText("S - 1 Step", 1300, 800, 25, Color.BLACK);
+                Raylib.DrawText("P - Step Every Frame on/off", 1300, 830, 25, Color.BLACK);
+                Raylib.DrawText("Space - Reset", 1300, 860, 25, Color.BLACK);
+                Raylib.DrawFPS(1500, 960);
+                ts.Draw();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
     }
 }
---
+++ TriangleLife.cs
@@ -0,0 +1,151 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace AttractionZero;
+
+public class TriangleLife
+{
+    private byte[] _backField;
+    private byte[] _drawField;
+
+    public const float TriangleWidthInPixels = 41f/5;
+    public const float TriangleHeightInPixels = 71f/5;
+
+
+    public int Width { get; }
+    public int Height { get; }
+
+    public TriangleLife(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        int numberOfBytes = (int)((width * height) / 8);
+        if ((width * height) % 8 != 0)
+            numberOfBytes += 1;
+
+        _backField = new byte[numberOfBytes];
+        _drawField = new byte[numberOfBytes];
+        Random.Shared.NextBytes(_backField);
+        _backField.CopyTo(_drawField, 0);
+    }
+
+    private void Flip() => (_drawField, _backField) = (_backField, _drawField);
+
+    private bool GetPixel(byte[] field, int i, int j)
+    {
+        if (i < 0 || i >= Width || j < 0 || j >= Height)
+            return false;
+        int index = i * Height + j;
+        int byteIndex = index / 8;
+        int bitIndex = index % 8;
+        return (field[byteIndex] & (1 << bitIndex)) != 0;
+    }
+
+    private void SetPixel(byte[] field, int i, int j, bool value)
+    {
+        if (i < 0 || i >= Width || j < 0 || j >= Height)
+            return;
+        int index = i * Height + j;
+        int byteIndex = index / 8;
+        int bitIndex = index % 8;
+        if (value)
+            field[byteIndex] |= (byte)(1 << bitIndex);
+        else
+            field[byteIndex] &= (byte)~(1 << bitIndex);
+    }
+
+    public void Draw()
+    {
+
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                Vector2 center = new Vector2(TriangleWidthInPixels + i * TriangleWidthInPixels,
+                    TriangleHeightInPixels/2 + j * TriangleHeightInPixels);
+
+                Vector2 a, b, c;
+                if ((i + j) % 2 == 0)
+                {
+                    a = center + new Vector2(0, -TriangleHeightInPixels / 2);
+                    b = center + new Vector2(-TriangleWidthInPixels, TriangleHeightInPixels / 2);
+                    c = center + new Vector2(TriangleWidthInPixels, TriangleHeightInPixels / 2);
+                }
+                else
+                {
+                    a = center + new Vector2(0, TriangleHeightInPixels / 2);
+                    b = center + new Vector2(TriangleWidthInPixels, -TriangleHeightInPixels / 2);
+                    c = center + new Vector2(-TriangleWidthInPixels, -TriangleHeightInPixels / 2);
+                }
+                if (GetPixel(_drawField, i,j))
+                {
+                    Raylib.DrawTriangle(a, b, c, Color.BLACK);
+                }
+                else
+                {
+                    // Raylib.DrawTriangleLines(a, b, c, Color.BLACK);
+                }
+            }
+        }
+            
+    }
+
+    public void Step()
+    {
+        Array.Fill(_backField, (byte)0);
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                int s = 0;
+                if ((i + j) % 2 == 0)
+                {
+                    if (GetPixel(_drawField, i - 1, j)) s += 12;
+                    if (GetPixel(_drawField, i + 1, j)) s += 12;
+                    if (GetPixel(_drawField, i, j + 1)) s += 12;
+                        
+                    if (GetPixel(_drawField, i - 1, j - 1)) s += 4;
+                    if (GetPixel(_drawField, i + 1, j - 1)) s += 4;
+                    if (GetPixel(_drawField, i - 2, j)) s += 4;
+                    if (GetPixel(_drawField, i + 2, j)) s += 4;
+                    if (GetPixel(_drawField, i - 1, j + 1)) s += 4;
+                    if (GetPixel(_drawField, i + 1, j + 1)) s += 4;
+
+                    if (GetPixel(_drawField, i, j - 1)) s += 3;
+                    if (GetPixel(_drawField, i - 2, j + 1)) s += 3;
+                    if (GetPixel(_drawField, i + 2, j + 1)) s += 3;
+                }
+                else
+                {
+                    if (GetPixel(_drawField, i - 1, j)) s += 12;
+                    if (GetPixel(_drawField, i + 1, j)) s += 12;
+                    if (GetPixel(_drawField, i, j - 1)) s += 12;
+
+                    if (GetPixel(_drawField, i - 1, j - 1)) s += 4;
+                    if (GetPixel(_drawField, i + 1, j - 1)) s += 4;
+                    if (GetPixel(_drawField, i - 2, j)) s += 4;
+                    if (GetPixel(_drawField, i + 2, j)) s += 4;
+                    if (GetPixel(_drawField, i - 1, j + 1)) s += 4;
+                    if (GetPixel(_drawField, i + 1, j + 1)) s += 4;
+
+                    if (GetPixel(_drawField, i, j + 1)) s += 3;
+                    if (GetPixel(_drawField, i - 2, j - 1)) s += 3;
+                    if (GetPixel(_drawField, i + 2, j - 1)) s += 3;
+                }
+
+                if (GetPixel(_drawField, i, j))
+                {
+                    if (s > 13 && s < 32)
+                        SetPixel(_backField, i, j, true);
+                }
+                else
+                {
+                    if (s > 21 && s < 32)
+                        SetPixel(_backField, i, j, true);
+                }
+            }
+        }
+
+        Flip();
+    }
+}
